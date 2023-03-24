d56d673ecd9f181779381ed9104b6ed3e89ca63e
Ashish Khare <akstyles211@gmail.com>
2023-03-24 19:43:00 +0530

    Init commit -primer

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ import Projects from './container/Projects'
 import Stuff from './container/Stuff'
 import Footer from './container/Footer'
 
+import {ThemeProvider, theme} from '@primer/react'
+import deepmerge from 'deepmerge'
+
+const customTheme = deepmerge(theme, {
+  fonts: {
+    mono: `'Roboto Mono', monospace`,
+  },
+})
+
 function App(){
 	return (
-		<>
+		<ThemeProvider theme={customTheme}>
 			<Intro />
 			<ul className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 p-4 mt-8">
 				<Stuff />
@@ -14,7 +23,7 @@ function App(){
 				<Minis />
 			</ul>
 			<Footer/>
-		</>
+		</ThemeProvider>
 	)
 }
 
--- src/container/Intro.jsx
+++ src/container/Intro.jsx
@@ -1,4 +1,4 @@
-import { Circle, GithubLogo, RedditLogo, TwitterLogo, SpotifyLogo } from '@phosphor-icons/react'
+import { SealCheck, Circle, GithubLogo, RedditLogo, TwitterLogo, SpotifyLogo } from '@phosphor-icons/react'
 
 import selfie from '../assets/hero_halftone.png'
 import logo from '../assets/logo.png'
@@ -16,10 +16,12 @@ export default function Intro(props){
 				<div className="m-auto v-flex gap-6">
 					<div className="w-full">
 						{/*<img src={logo} alt="Octoicon's mention svg" className="w-12 h-12 ml-2" />*/}
-						<Circle className="w-12 h-12 ml-2 -mb-2" />
+						{/*<Circle className="w-12 h-12 ml-2 -mb-2" />*/}
+						{/*<SealCheck className="w-12 h-12 ml-2 -mb-2" />*/}
 					</div>
 					<h1 className="text-8xl tracking-tight">
 						Ashish Khare is mad!
+						<SealCheck className="inline w-16 h-16 ml-4 mb-4 my-auto" />
 					</h1>
 					<p className="mb-8 text-lg">
 						Hello everyone! I’m Ashish Khare, a indie web developer and designer based in India. Basically, writing this from my table. I envy VDOM creators for thinking a concept like itself. Also, you might find me across these places.
@@ -29,13 +31,12 @@ export default function Intro(props){
 						<a href="https://github.com/hugekontrast" className="p-3">
 							<GithubLogo />
 						</a>
-						<a href="https://open.spotify.com/user/nwo0j3ix1j5mbwl0l3anytu29" className="p-3 flex items-center gap-1 w-full">
+						<a href="https://open.spotify.com/user/nwo0j3ix1j5mbwl0l3anytu29" className="p-3">
 							<SpotifyLogo weight="fill" fill="#1DB954" className="w-8 h-8" />
-							<marquee>listening to <b>The Hills by Weeknd</b></marquee>
 						</a>
-						<a href="https://twitter.com/intent/tweet?screen_name=AshishK1331&ref_src=twsrc%5Etfw" className=" button-primary ml-auto" data-text="Hey!" data-related="" data-show-count="false">
-							<TwitterLogo weight="fill" className="w-16 h-16 aspect-square" />
-							<p className="font-medium">Tweet</p>
+						<marquee className="w-full">listening to <b>The Hills by Weeknd</b></marquee>
+						<a href="https://twitter.com/intent/tweet?screen_name=AshishK1331&ref_src=twsrc%5Etfw" className="twitter-mention-button button-primary ml-auto" data-text="Hey!" data-related="" data-show-count="false">
+							<p className="font-medium m-auto">Tweet</p>
 						</a>
 						<script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
 					</div>
